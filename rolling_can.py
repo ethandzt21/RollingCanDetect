d75b37fdfb631b33caf0c0a8c5329cbf3bf4b7f3
Ethan Deng <91970230+ethandeng21@users.noreply.github.com>
2022-02-14 22:55:01 -0500

    Update rolling_can.py

--- rolling_can.py
+++ rolling_can.py
@@ -2,23 +2,25 @@ import cv2
 import numpy as np
 
 
-can = cv2.VideoCapture("/Users/local/PycharmProjects/RollingCanDetection/test/RollingCan2.mp4")
+can = cv2.VideoCapture("test/RollingCan2.mp4")
 font = cv2.FONT_HERSHEY_SIMPLEX
 total_frames = int(can.get(cv2.CAP_PROP_FRAME_COUNT))
 
 counter = 0
 while can.isOpened():
     _, frame = can.read()
-    frame = frame[300:1300, 0:10000]
-    blur = cv2.medianBlur(frame, 21)
+    frame = cv2.resize(frame, (1280, 720))
+    frame = frame[50:550, :]
+
+    blur = cv2.medianBlur(frame, 11)
     gray_scale = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
-    circles = cv2.HoughCircles(gray_scale, cv2.HOUGH_GRADIENT, 1, 100, param1=80, param2=30, minRadius=230, maxRadius=250)
+    circles = cv2.HoughCircles(gray_scale, cv2.HOUGH_GRADIENT, 1, 100, param1=80, param2=30, minRadius=75, maxRadius=85)
 
-    cv2.putText(frame, "FRAMES OF CIRCLE DETECTED: ", (50, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
-    cv2.putText(frame, str(counter), (540, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
-    cv2.putText(frame, "/", (590, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
-    cv2.putText(frame, str(total_frames), (620, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, "FRAMES OF CIRCLE DETECTED: ", (25, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, str(counter), (515, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, "/", (575, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
+    cv2.putText(frame, str(total_frames), (600, 50), font, 1, (255, 0, 255), 2, cv2.LINE_4)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
@@ -26,9 +28,9 @@ while can.isOpened():
 
         for i in circles[0, :]:
             # draw outer circle
-            cv2.circle(frame, (i[0], i[1]), i[2], (255, 0, 0), 10)
+            cv2.circle(frame, (i[0], i[1]), i[2], (255, 0, 0), 2)
             # draw center of the circle
-            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 0), 10)
+            cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 0), 3)
 
             break
 
